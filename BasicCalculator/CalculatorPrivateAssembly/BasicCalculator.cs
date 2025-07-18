19e5203085165493b2a28c61e9c5f53f86a23372
Mark <markmanalo1717@gmail.com>
2025-07-17 22:31:09 -1200

    Fixing duplication

--- BasicCalculator/CalculatorPrivateAssembly/BasicCalculator.cs
+++ BasicCalculator/CalculatorPrivateAssembly/BasicCalculator.cs
@@ -6,5 +6,5 @@ public static class BasicCalculator
     public static float Subtraction(float a, float b) => a - b;
     public static float Multiplication(float a, float b) => a * b;
     public static float Division(float a, float b) => b != 0 ? a / b : 
-        throw new DivideByZeroException("Cannot divide by zero.");
+        throw new DivideByZeroException("Cannot divide by zero sorry.");
 }
